d5a81ae45de4d84dd2d2c7c640215a0fc0bba2b0
Maxime Christiaens <maxime.ad.christiaens@gmail.com>
2018-12-03 12:31:03 +0100

    init commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Formu For Hackers Poulettes</title>
+    <!-- Materialize CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+</head>
+<body>
+    <div class="row">
+        <form action="" class="col s12">
+            <div class="row">
+                <div class="input-field col s3">
+                    <input name="prenom" id="prenom" type="text" class="validate">
+                    <label for="prenom">Prenom</label>
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+<!-- Materialize JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</html>
