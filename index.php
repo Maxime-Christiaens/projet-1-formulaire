5ce0fef497c73b1b32628966c83dfed9dc280776
Maxime Christiaens <maxime.ad.christiaens@gmail.com>
2018-12-04 10:18:52 +0100

    On progresse good

--- index.php
+++ index.php
@@ -10,52 +10,58 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 </head>
 <body>
-    <?php
-    include "php/formu.php";
-    ?>
+
     <div class="row bg-color formuDiv">
         <div class="col s4 push-s4 pull-s4">
             <img class="responsive-img" src="imgs/hackers-poulette-logo.png" alt="Logo de hackers poulette">
         </div>
-        <form action="index.php" method="POST" class="col s10 offset-s1">
+        <form action="php/formu.php" method="POST" class="col s10 offset-s1">
             <div class="row">
             </div>
             <div class="row">
                 <div class="input-field col s3 push-s2">
-                    <input name="prenom" id="prenom" type="text" class="validate" required="required">
+                    <input value="<?php session_start(); echo($_SESSION["prenom"]); ?>" name="prenom" id="prenom" type="text" class="validate" required="required">
                     <label for="prenom">Prenom</label>
                 </div>
                 <div class="input-field col s3 push-s4">
-                    <input name="nom" id="nom" type="text" class="validate" required="required">
+                    <input value="<?php session_start(); echo($_SESSION["nom"]); ?>" name="nom" id="nom" type="text" class="validate" required="required">
                     <label for="nom">Nom</label>
                 </div>
             </div>
             <div class="row">
                 <div class="input-field col s8 push-s2">
-                    <input name="email" type="email" id="email" class="validate" required="required">
+                    <input value="<?php session_start(); echo($_SESSION["email"]); ?>" name="email" type="email" id="email" class="validate" required="required">
                     <label for="email">E-mail</label>
                 </div>
             </div>
             <div class="row">
                 <div class="input-field col s2 push-s2">
-                    <select name="choix" id="choix">
+                    <select multiple value="<?php session_start(); echo($_SESSION["choix"]); ?>" name="choix" id="choix">
                         <optgroup label="Quelle est ton choix ?">
                             <option value="Option 1">Option 1</option>
                             <option value="Option 2">Option 2</option>
-                            <option value="Option 3">Option 2</option>
+                            <option value="Option 3">Option 3</option>
+                            <option value="Option 4">Option 4</option>
+                            <option value="Option 5">Option 5</option>
                         </optgroup>
                     </select>
                     <label for="choix">Quel est ton choix ?</label>
                 </div>
                 <div class="input-field col s2 push-s3">
-                    <input name="country" id="country" type="text" class="validate">
+                    <input value="<?php session_start(); echo($_SESSION["country"]); ?>" name="country" id="country" type="text" class="validate" required="required">
                     <label for="country">Pays</label>
                 </div>
                 <div class="input-field col s2 push-s4">
-                    <select name="sexe" id="sexe">
+                    <select value="<?php session_start(); echo($_SESSION["sexe"]); ?>" name="sexe" id="sexe">
                         <optgroup label="Quelle est ton genre ?">
                             <option value="Homme">Masculin</option>
-                            <option value="Femme">Féminin</option>
+                            <option value="Femme" 
+                            <?php 
+                            session_start();
+                            if ($_SESSION["sexe"] == "Femme"){
+                                echo("selected");
+                            }
+                            ?> >Féminin</option>
                         </optgroup>
                     </select>
                     <label for="sexe">Genre</label>
@@ -63,12 +69,14 @@
             </div>
             <div class="row">
                 <div class="input-field col s8 push-s2">
-                    <textarea class="materialize-textarea" name="message" id="message" required="required"></textarea>
+                    <textarea value="Bonsoir" class="materialize-textarea" name="message" id="message" required="required"><?php session_start(); echo($_SESSION["message"]); ?></textarea>
                     <label for="message">Un message à nous transmettre ?</label>
                 </div>
             </div>
             <div class="row">
-                <button class=" col s2 push-s5 waves-effect waves-light btn" type="submit">Envoyez le formulaire</button>
+                <button class=" col s2 push-s5 waves-effect waves-light btn" type="submit"> 
+                    Envoyez le formulaire
+                </button>
             </div>
         </form>
     </div>
--- php/formu.php
+++ php/formu.php
@@ -1,72 +1,51 @@
-<?php 
+<?php
 
-//isset si variable existe renvoie true 
-if (isset($_POST["prenom"]) && isset($_POST["nom"]) && isset($_POST["email"]) && isset($_POST["choix"]) && isset($_POST["country"]) && isset($_POST["sexe"]) && isset($_POST["message"])){ //condition vérifiant si tout à été rempli
+//////////////////////////
+//Créations des sessions//
+//////////////////////////
+session_start();
+$_SESSION["prenom"] = $_POST["prenom"];
+$_SESSION["nom"] = $_POST["nom"];
+$_SESSION["email"] = $_POST["email"];
+$_SESSION["choix"] = $_POST["choix"];
+$_SESSION["sexe"] = $_POST["sexe"];
+$_SESSION["country"] = $_POST["country"];
+$_SESSION["message"] = $_POST["message"];
+
+echo($_SESSION["sexe"]);
+
+////////////
+//Filtrage//
+////////////
+
+//isset si variable existe renvoie true
+if (isset($_POST["prenom"]) && isset($_POST["nom"]) && isset($_POST["email"]) && isset($_POST["choix"]) && isset($_POST["country"]) && isset($_POST["sexe"]) && isset($_POST["message"])) { //condition vérifiant si tout à été rempli
 
     $options = array(
-        'prenom' 	=> FILTER_SANITIZE_STRING,
-        'nom' 	=> FILTER_SANITIZE_STRING,
-        'email' 		=> FILTER_VALIDATE_EMAIL,
-        'country' 		=> FILTER_SANITIZE_STRING,
-        'sexe' 		=> FILTER_SANITIZE_STRING,
-        'message' 		=> FILTER_SANITIZE_STRING);
+        "prenom" => FILTER_SANITIZE_STRING,
+        "nom" => FILTER_SANITIZE_STRING,
+        "email" => FILTER_VALIDATE_EMAIL,
+        "country" => FILTER_SANITIZE_STRING,
+        "sexe" => FILTER_SANITIZE_STRING,
+        "message" => FILTER_SANITIZE_STRING);
 
     $result = filter_input_array(INPUT_POST, $options);
 
-    if ($result != null AND $result != FALSE) {
+    if ($result != null and $result != false) {
         echo "Tous les champs ont été nettoyés !<br>";
     } else {
         echo "Un champ est vide ou n'est pas correct!";
-    }  
+    }
     //vérifie si la désinféction a été efféctué avec succés
 
-    foreach ($result as $key => $value){
-        echo("$key = ".$result[$key]." <br>");
-    }
+    /* foreach ($result as $key => $value) {
+        echo ("$key = " . $result[$key] . " <br>");
+    } */
     //affiche les valeurs de tout les inputs du formulaires
-    
+
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ?>
+<a href="../index.php">
+    Retour
+</a>
